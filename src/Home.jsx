1262a836f009c2e331bfed26b3513bd614e88339
SaphireVert <jerome.cosandey@gmail.com>
2023-07-28 12:21:51 +0200

    feat: display array

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,20 +1,43 @@
 import React from 'react'
 
+let array = []
+
+for (let index = 0; index < 1000; index++) {
+    let result = ''
+    const characters =
+    'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+    const charactersLength = 10
+    let counter = 0
+    while (counter < charactersLength) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength))
+        counter += 1
+    }
+    array.push(result)
+}
+console.dir(array)
+
 const myWorker = new Worker('./src/worker.js')
-myWorker.onmessage = function() {
+myWorker.postMessage(array)
+
+myWorker.onmessage = function () {
     console.log('Message received from worker')
 }
 function Home() {
-    
-    const runSort = async () => {
-        myWorker.postMessage([5, 7])
-
-    }
+    const runSort = async () => {}
 
     return (
-        <button type="button" onClick={runSort}>
-      Run Sort
-        </button>
+        <>
+            <button type="button" onClick={runSort}>
+          Run Sort
+            </button>
+            <div></div>
+            {array.map((x, i) => (
+                <>
+                    <span key={i}>{x}</span>
+                    <br />
+                </>
+            ))}
+        </>
     )
 }
 
