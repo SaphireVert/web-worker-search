607d38ed622e85f90a9aa641b893bc19e4c4f0fa
SaphireVert <jerome.cosandey@gmail.com>
2023-07-28 11:56:45 +0200

    feat: basic worker working

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
 
+const myWorker = new Worker('./src/worker.js')
+myWorker.onmessage = function() {
+    console.log('Message received from worker')
+}
 function Home() {
+    
+    const runSort = async () => {
+        myWorker.postMessage([5, 7])
+
+    }
+
     return (
-        <div>Homepage</div>
+        <button type="button" onClick={runSort}>
+      Run Sort
+        </button>
     )
 }
 
